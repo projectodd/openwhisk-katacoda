294054cc419a6eaad44e0a3563dc54ad629d0f10
Jim Curtis <jim.curtis@redhat.com>
2018-04-23 17:12:59 -0700

    Update pre-scenario-setup.sh

--- simple-nodejs-greeting/pre-scenario-setup.sh
+++ simple-nodejs-greeting/pre-scenario-setup.sh
@@ -16,39 +16,28 @@ oc new-project faas --display-name="FaaS- OpenShift Cloud Functions"
 oc adm policy add-role-to-user admin developer -n faas
 oc process -f https://git.io/openwhisk-template | oc create -f -
 
-# PASSED=false
-# TIMEOUT=0
-# until $PASSED || [ $TIMEOUT -eq 60 ]; do
-#   OC_DEPLOY_STATUS=$(oc get pods -o wide | grep "controller-0" | awk '{print $3}')
-#   if [ "$OC_DEPLOY_STATUS" == "Running" ]; then
-#     PASSED=true
-#     break
-#   fi
-#   let TIMEOUT=TIMEOUT+1
-#   sleep 3
-# done
-# PASSED=false
-# TIMEOUT=0
-# until $PASSED || [ $TIMEOUT -eq 5 ]; do
-#   INVOKER_HEALTH=$(oc logs controller-0 -n faas | grep "invoker status changed" | grep " Healthy" | awk '{print $11}')
-#   if [ "$INVOKER_HEALTH" == "Healthy" ]; then
-#     PASSED=true
-#     break
-#   fi
-#   let TIMEOUT=TIMEOUT+1
-#   sleep 2
-# done
 PASSED=false
 TIMEOUT=0
 until $PASSED || [ $TIMEOUT -eq 60 ]; do
-  SUCCESSFUL_JOB=$(oc get jobs -o wide | grep "$1" | awk '{print $3}')
-    if [ "$SUCCESSFUL_JOB" == "1" ]; then
-      PASSED=true
-      break
-    fi
-    let TIMEOUT=TIMEOUT+1
-    sleep 5
-  done
+  OC_DEPLOY_STATUS=$(oc get pods -o wide | grep "controller-0" | awk '{print $3}')
+  if [ "$OC_DEPLOY_STATUS" == "Running" ]; then
+    PASSED=true
+    break
+  fi
+  let TIMEOUT=TIMEOUT+1
+  sleep 3
+done
+PASSED=false
+TIMEOUT=0
+until $PASSED || [ $TIMEOUT -eq 5 ]; do
+  INVOKER_HEALTH=$(oc logs controller-0 -n faas | grep "invoker status changed" | grep " Healthy" | awk '{print $11}')
+  if [ "$INVOKER_HEALTH" == "Healthy" ]; then
+    PASSED=true
+    break
+  fi
+  let TIMEOUT=TIMEOUT+1
+  sleep 2
+done
 
 oc patch route openwhisk --namespace faas -p '{"spec":{"tls": {"insecureEdgeTerminationPolicy": "Allow"}}}'
 AUTH_SECRET=$(oc get secret whisk.auth -o yaml | grep "system:" | awk '{print $2}' | base64 --decode)
