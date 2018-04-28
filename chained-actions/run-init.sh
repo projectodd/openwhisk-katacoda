41e8fd84a98ed159f78852c219231b31dc80ef9c
Jim Curtis <jim.curtis@redhat.com>
2018-04-27 22:09:43 -0700

    Update run-init.sh

--- chained-actions/run-init.sh
+++ chained-actions/run-init.sh
@@ -7,3 +7,8 @@ ssh root@host01 "tar -zxvf /root/installation/OpenWhisk_CLI-latest-linux-386.tgz
 ssh root@host01 "oc new-project faas --display-name='FaaS - OpenShift Cloud Functions'"
 ssh root@host01 "oc adm policy add-role-to-user admin developer -n faas"
 ssh root@host01 "oc process -f https://git.io/vpnUR | oc create -f -"
+ssh root@host01 "git clone https://github.com/apache/incubator-openwhisk-devtools openwhisk-devtools"
+ssh root@host01 "cd openwhisk-devtools/java-action-archetype \
+    && mvn -DskipTests clean install  \
+    && cd  \
+    && rm -rf /tmp/openwhisk-devtools"
