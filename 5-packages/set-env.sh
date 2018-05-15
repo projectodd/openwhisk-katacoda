44eebb4ed4433b17a9451bc42dd387580f7e70a4
Justin Lee <julee@redhat.com>
2018-05-15 10:44:56 -0400

    update directory names to be consistent with ordering on openshift-labs/learn-katacoda

--- 5-packages/set-env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-<<<<<<< HEAD
-=======
-sleep 1
->>>>>>> we don't need that PATH entry
-/root/setup-environment.sh
--- 6-packages/set-env.sh
+++ 6-packages/set-env.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+<<<<<<< HEAD
+=======
 sleep 1
+>>>>>>> we don't need that PATH entry
 /root/setup-environment.sh
